71f7e0dd15a3b3ff57739367ccb1c795b4b486ad
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2024-07-06 10:45:37 +0530

    files Added

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,33 @@
+
+
+
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      axios.get('https://sleath-backend.vercel.app/api/auth/me', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      .then(response => {
+        setUser(response.data.data);
+      })
+      .catch(error => {
+        console.error("Error validating token", error);
+        localStorage.removeItem('authToken');
+      });
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
